5d0d702d4d2c728fdbe36c6b40ca4ff5598f1f08
🌐 UtrechtOG 🌐 <Calimali2006@gmail.com>
2026-02-01 16:11:44 +0100

    Update enemy.js

--- enemy.js
+++ enemy.js
@@ -1,1 +1,16 @@
+let enemies = [{ x: 7, y: 8 }];
 
+function moveEnemies() {
+    enemies.forEach(enemy => {
+        const dx = Math.sign(player.x - enemy.x);
+        const dy = Math.sign(player.y - enemy.y);
+
+        if (map[enemy.y + dy][enemy.x] === 0) enemy.y += dy;
+        else if (map[enemy.y][enemy.x + dx] === 0) enemy.x += dx;
+
+        if (enemy.x === player.x && enemy.y === player.y) {
+            alert("Caught!");
+            resetGame();
+        }
+    });
+}
