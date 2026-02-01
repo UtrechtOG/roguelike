1acc068d5e9a3b1aca58db75afbbcefee392dc93
🌐 UtrechtOG 🌐 <Calimali2006@gmail.com>
2026-02-01 16:11:20 +0100

    Update player.js

--- player.js
+++ player.js
@@ -1,1 +1,13 @@
+const player = { x: 1, y: 1 };
 
+function movePlayer(dx, dy) {
+    const nx = player.x + dx;
+    const ny = player.y + dy;
+
+    if (map[ny][nx] !== 1) {
+        player.x = nx;
+        player.y = ny;
+    }
+
+    if (map[ny][nx] === 2) nextRoom();
+}
